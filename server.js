6481c444aa59522b734e94180c3741d45a203736
lukechidubem <lukechidubem@gmail.com>
2022-11-12 21:02:47 +0100

    updated server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ console.log(__dirname, path.join(__dirname, "build"));
 const app = express(),
   port = process.env.PORT || 3002;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 app.listen(port, (_) => console.log("Server running on port", port));
